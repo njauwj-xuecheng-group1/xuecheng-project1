87714a2172cbb54e5ae749a80cd35af23bd4aa5b
wj <1724031211@qq.com>
2023-05-02 13:45:18 +0800

    swagger生成接口文档

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -1,13 +1,13 @@
-package com.xuecheng.content.api;
+package com.xuecheng;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XuechengPlusContentApiApplication {
-
+@EnableSwagger2Doc
+public class ContentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(XuechengPlusContentApiApplication.class, args);
+        SpringApplication.run(ContentApplication.class, args);
     }
-
 }
---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -0,0 +1,42 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wj
+ * @version 1.0
+ * @description 课程信息编辑接口
+ * @date 2022/9/6 11:29
+ */
+@RestController
+@Api(value = "课程信息管理", tags = "课程信息管理接口")
+public class CourseBaseInfoController {
+
+    @ApiOperation(value = "课程信息查询")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams) {
+
+        CourseBase courseBase = new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBases = new ArrayList();
+        courseBases.add(courseBase);
+        PageResult pageResult = new PageResult<CourseBase>(courseBases, 10, 1, 10);
+        return pageResult;
+
+
+    }
+
+}
