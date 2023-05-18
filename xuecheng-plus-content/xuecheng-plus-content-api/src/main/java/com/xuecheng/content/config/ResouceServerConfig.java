9789907cfc163d7f6aa3b1cf395c30ee713a18eb
wj <1724031211@qq.com>
2023-05-18 21:10:15 +0800

    网关认证

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/config/ResouceServerConfig.java
@@ -0,0 +1,41 @@
+package com.xuecheng.content.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+    //资源服务标识,不能乱写与认证服务得保持一致
+    public static final String RESOURCE_ID = "xuecheng-plus";
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID)//资源 id
+                .tokenStore(tokenStore)
+                .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                //所有/r/**的请求必须认证通过,每一个服务都要认证的话太麻烦，交给网关统一认证
+//                .antMatchers("/r/**", "/course/**").authenticated()
+                .anyRequest().permitAll()
+        ;
+    }
+
+}
